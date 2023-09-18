c162d9493e07553b392fff52dd4c1a8d325a3b27
Minh Chau Luu <87756495+evlynxx@users.noreply.github.com>
2023-09-18 14:28:19 +1000

    Update streamlit_app.py
    imported a txt file & used streamlit to display the data on web

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,7 @@
 import streamlit
+import pandas as pd
 
+# First part of the new menu
 streamlit.title('My Mom\'s New Healthy Diner')
 
 streamlit.header('Breakfast Favorites')
@@ -9,3 +11,7 @@ streamlit.text('🐔 Hard-Boiled Free-Range Egg')
 streamlit.text('🥑🍞 Avocado Toast')
 
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
+
+# Import txt file
+my_fruit_list = pd.read_csv('https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt')
+streamlit.dataframe(my_fruit_list)
